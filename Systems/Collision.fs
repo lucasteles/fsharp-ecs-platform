828c125de9719a7972514fd7b3bfbf93c4505782
Lucas Teles <lucas.teles@outlook.com>
2021-12-04 23:55:08 -0300

    WIP

--- Systems/Collision.fs
+++ Systems/Collision.fs
@@ -25,11 +25,31 @@ module private Systems =
                             | true, {Position=Position pos} -> collider.ColliderBounds.OffsetValue(pos)
                             | false, _ -> collider.ColliderBounds
                         let actorRectangle = actorCollider.ColliderBounds.OffsetValue(position)
-                        if (eid <> colliderEid && actorRectangle.Intersects(colliderRectangle))
-                        then
+                        if (eid <> colliderEid && actorRectangle.Intersects(colliderRectangle)) then
+
+                            let horizontal =
+                                if (actorRectangle.Left >= colliderRectangle.Left
+                                   && actorRectangle.Left <= colliderRectangle.Right)
+                                   || (actorRectangle.Right >= colliderRectangle.Left
+                                   && actorRectangle.Right <= colliderRectangle.Right)
+                                   || (actorRectangle.Left <= colliderRectangle.Left
+                                   && actorRectangle.Right >= colliderRectangle.Right)
+                                then 1f else 0f
+
+                            let vertical =
+                                if (actorRectangle.Top >= colliderRectangle.Top
+                                   && actorRectangle.Top <= colliderRectangle.Bottom)
+                                   || (actorRectangle.Bottom >= colliderRectangle.Left
+                                   && actorRectangle.Bottom <= colliderRectangle.Right)
+                                   || (actorRectangle.Top <= colliderRectangle.Top
+                                   && actorRectangle.Bottom >= colliderRectangle.Bottom)
+                                then 1f else 0f
+
                             let transformRef = &actor.Value1
                             transformRef <- {  transform
-                                               with Position = Position (position - velocity * e.DeltaTime.seconds) }
+                                               with Position =
+                                                       Position (position - velocity * Vector2(horizontal, vertical) * e.DeltaTime.seconds) }
+
                             let velocityRef = &actor.Value2
                             velocityRef <- Velocity Vector2.Zero
                             world.Send({ Game = e.Game
